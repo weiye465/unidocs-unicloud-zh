b4e66482644b1080a1b12bd3054fa1be7e6e8b14
DCloud_LXH <283700113@qq.com>
2023-12-11 17:31:19 +0800

    feat: sitemap

--- docs/.vuepress/markdown/createSiteMap.js
+++ docs/.vuepress/markdown/createSiteMap.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-const domain = 'https://zh.uniapp.dcloud.io'
+const domain = 'https://doc.dcloud.net.cn/uniCloud'
 const xmlBefore = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 `
