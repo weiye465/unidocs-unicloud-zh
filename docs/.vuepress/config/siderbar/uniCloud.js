d6a20b02fcc543ffd15731040968df31e2a600d8
DCloud_LXH <283700113@qq.com>
2023-12-20 15:28:07 +0800

    chore: update theme to 1.4.1

--- docs/.vuepress/config/siderbar/uniCloud.js
+++ docs/.vuepress/config/siderbar/uniCloud.js
@@ -1,65 +1,81 @@
 export default {
-	contactItems: [{
-			name: 'uniCloud Web控制台',
-			url: 'https://unicloud.dcloud.net.cn',
-			imageUrl: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/cloud@2x.jpg'
-		},
+	contacts: [
 		{
-			name: '插件市场',
-			url: 'https://ext.dcloud.net.cn/',
-			imageUrl: 'https://img-cdn-aliyun.dcloud.net.cn/dev/img/ext/favicon.png'
-		},
-		{
-			name: 'ask问答社区',
-			url: 'https://ask.dcloud.net.cn/explore/',
-			imageUrl: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/d@2x.png'
-		},
-		{
-			name: 'uniAD',
-			url: 'https://uniad.dcloud.net.cn',
-			imageUrl: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uniad-520.png'
-		},
-		{
-			name: 'uni统计',
-			url: 'https://tongji.dcloud.net.cn/',
-			imageUrl: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uni-tongji-icon.png'
+			type: 'link',
+			items: [
+				{
+					name: 'uniCloud Web控制台',
+					url: 'https://unicloud.dcloud.net.cn',
+					imageUrl: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/cloud@2x.jpg',
+				},
+				{
+					name: '插件市场',
+					url: 'https://ext.dcloud.net.cn/',
+					imageUrl: 'https://img-cdn-aliyun.dcloud.net.cn/dev/img/ext/favicon.png',
+				},
+				{
+					name: 'ask问答社区',
+					url: 'https://ask.dcloud.net.cn/explore/',
+					imageUrl: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/d@2x.png',
+				},
+				{
+					name: 'uniAD',
+					url: 'https://uniad.dcloud.net.cn',
+					imageUrl:
+						'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uniad-520.png',
+				},
+				{
+					name: 'uni统计',
+					url: 'https://tongji.dcloud.net.cn/',
+					imageUrl:
+						'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uni-tongji-icon.png',
+				},
+				{
+					name: 'uni-im交流群',
+					imageUrl:
+						'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/chat-icon-r.png',
+					url: 'https://im.dcloud.net.cn/#/?joinGroup=658160eb3e585b555acebfd7',
+				},
+			]
 		}
 	],
-	qq_group: [{
-			"number": "1012245137",
-			"state": 1,
+	qq_group: [
+		{
+			number: '1012245137',
+			state: 1,
 			attendance: 2000,
-			"prefix": "群1"
+			prefix: '群1',
 		},
 		{
-			"number": "749911289",
-			"state": 1,
+			number: '749911289',
+			state: 1,
 			attendance: 1000,
-			"prefix": "群2"
+			prefix: '群2',
 		},
 		{
-			"number": "599819864",
-			"state": 1,
+			number: '599819864',
+			state: 1,
 			attendance: 1000,
-			"prefix": "群3"
+			prefix: '群3',
 		},
 		{
-			"number": "641753405",
-			"state": 1,
+			number: '641753405',
+			state: 1,
 			attendance: 1000,
-			"prefix": "群4"
+			prefix: '群4',
 		},
 		{
-			"number": "741357947",
-			"state": 1,
+			number: '741357947',
+			state: 1,
 			attendance: 1000,
-			"prefix": "群6"
+			prefix: '群6',
 		},
 		{
-			"number": "806429316",
-			"state": 0,
-			"prefix": "群5",
-			joinQQGroupHref: 'https://qm.qq.com/cgi-bin/qm/qr?k=D2jzH0skhD1HmnlRrwlyBbqxRWStFeMj&jump_from=webapi&authKey=5s8Hm/RQxMRrfmGxRtRr1USy613azNDGuCYj9OqzrTTk8S8qebnM0N3gbLdDn/4l'
-		}
-	]
-}
+			number: '806429316',
+			state: 0,
+			prefix: '群5',
+			joinQQGroupHref:
+				'https://qm.qq.com/cgi-bin/qm/qr?k=D2jzH0skhD1HmnlRrwlyBbqxRWStFeMj&jump_from=webapi&authKey=5s8Hm/RQxMRrfmGxRtRr1USy613azNDGuCYj9OqzrTTk8S8qebnM0N3gbLdDn/4l',
+		},
+	],
+};
